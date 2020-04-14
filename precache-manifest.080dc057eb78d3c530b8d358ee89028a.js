42875a841b7f9f5b632d65e498d30823439a7d81
jacobkim9881 <jacobkim9881@gmail.com>
2020-04-14 17:51:00 +0900

    Updates

---
+++ precache-manifest.080dc057eb78d3c530b8d358ee89028a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed55e29132a162bbdae4f597bde33f3c",
+    "url": "/shoppingweb/index.html"
+  },
+  {
+    "revision": "72c39eec782d0183bd43",
+    "url": "/shoppingweb/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "62cc75d98d067067cfe0",
+    "url": "/shoppingweb/static/js/2.b8e0a936.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/shoppingweb/static/js/2.b8e0a936.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72c39eec782d0183bd43",
+    "url": "/shoppingweb/static/js/main.f2048726.chunk.js"
+  },
+  {
+    "revision": "fc59f3c7d34f56a29063",
+    "url": "/shoppingweb/static/js/runtime-main.cbf40abc.js"
+  }
+]);
